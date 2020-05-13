26234e279fdb31259d40d892dd20fd777b619cee
Wouter Symons <wosym@airsantelmo.com>
2020-05-14 01:00:27 +0200

    Add esc button to exit, draw some boxes

--- langtons_ant.go
+++ langtons_ant.go
@@ -5,11 +5,11 @@ import (
     "time"
 
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/gfx"
+	"github.com/yakshaveinc/go-keycodes"
 )
 
 const winWidth, winHeight int = 800, 600
-
-
 
 func main() {
     err := sdl.Init(sdl.INIT_EVERYTHING)
@@ -40,19 +40,36 @@ func main() {
     }
     defer tex.Destroy()
 
-    //pixels := make([]byte, winWidth*winHeight*4)
-
     var frameStart time.Time
     var elapsedTime float32
-    for {
+    var running bool = true
+    for running {
         frameStart = time.Now()
 
         for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-            switch event.(type) {
+            switch t := event.(type) {
                 case *sdl.QuitEvent:
                     return
+                case *sdl.KeyboardEvent:
+                    if(t.Type == sdl.KEYDOWN) {
+                        fmt.Println("key pressed: ", t.Keysym.Scancode)
+                        if uint16(t.Keysym.Scancode) == keycodes.KeyEscape{
+                            fmt.Println("escape pressed, exiting...")
+                            running = false
+                        }
+                    }
             }
         }
+
+        ret := gfx.BoxColor(renderer, 10, 10, 100, 100, sdl.Color{123, 50, 255, 100})
+        if ret != true {
+            fmt.Println("Error while drawing box")
+        }
+        ret = gfx.RectangleColor(renderer, 10, 10, 1000, 1000, sdl.Color{123, 50, 255, 100})
+        if ret != true {
+            fmt.Println("Error while drawing rect")
+        }
+
         renderer.Present()
 
 
